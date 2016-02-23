d4d15f421c6dd5d747bf57950accccf2b020c144
Casey West <casey@geeknest.com>
2016-02-23 14:08:50 -0800

    Pull fastly credentials from CF environment.

--- config/initializers/fastly.rb
+++ config/initializers/fastly.rb
@@ -1,5 +1,13 @@
 FastlyRails.configure do |c|
-  c.api_key = ENV['FASTLY_API_KEY']
-  c.service_id = ENV['FASTLY_SERVICE_ID']
+  begin
+    fastly = CF::App::Credentials
+      .find_all_by_all_service_name('fastly')
+      .first
+    c.api_key    = fastly.fetch('api_key')
+    c.service_id = fastly.fetch('service_id')
+  rescue
+    c.api_key = ENV['FASTLY_API_KEY']
+    c.service_id = ENV['FASTLY_SERVICE_ID']
+  end
 end
 
