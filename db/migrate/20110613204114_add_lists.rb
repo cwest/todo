c83f2dac101b3f82731aecc691ff7398c795e561
Casey West <casey@geeknest.com>
2016-02-23 13:15:32 -0800

    Update migrations to avoid mass assignment.

--- db/migrate/20110613204114_add_lists.rb
+++ db/migrate/20110613204114_add_lists.rb
@@ -2,20 +2,22 @@ class AddLists < ActiveRecord::Migration
   
   class List < ActiveRecord::Base
     has_many :tasks
+    attr_accessible :name
   end
 
   class Task < ActiveRecord::Base
     belongs_to :list
+    attr_accessible :name
   end
   
 
   def self.up
   
     list = List.create(:name => 'Welcome')
-    list.tasks.create(:name => 'Check out our docs https://support.cloud.engineyard.com/forums')
-    list.tasks.create(:name => 'Follow @EngineYard http://twitter.com/#!/engineyard')
-    list.tasks.create(:name => 'Follow @eycloud http://twitter.com/#!/eycloud')
-    list.tasks.create(:name => 'We blog http://www.engineyard.com/blog/')
+    list.tasks.create(:name => 'Check out our docs https://docs.cloudfoundry.org')
+    list.tasks.create(:name => 'Follow @pivotal https://twitter.com/pivotal')
+    list.tasks.create(:name => 'Follow @caseywest https://twitter.com/caseywest')
+    list.tasks.create(:name => 'We blog http://blog.pivotal.io')
     list.tasks.create(:name => 'Rock on!')
   end
 
