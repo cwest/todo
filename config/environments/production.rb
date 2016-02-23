554986ae5997994dbad856ce2acfe009f495984c
Casey West <casey@geeknest.com>
2016-02-23 11:53:17 -0800

    Set eager loading for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,6 @@ Listr::Application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+
+  config.eager_load = true
 end
